d129f5105f40922a416eca6165ed26ff242a11ee
alidehkhodaei <alidehkhodaei1998@gmail.com>
2023-09-26 20:21:47 +0330

    Add Constants file

---
+++ src/main/kotlin/constant/Constants.kt
@@ -0,0 +1,19 @@
+package constant
+/**
+ * Constants
+ *
+ * This package contains various constants used throughout the application.
+ **/
+const val ACCEPT_HEADER = "accept"
+const val JSON_CONTENT_TYPE = "application/json"
+const val CONTENT_TYPE = "Content-Type"
+const val TELEGRAM_API_BASE_URL = "https://api.telegram.org/"
+const val CHANNEL_ID = "@one_day_one_word_channel"
+const val POST_REQUEST = "POST"
+const val KEY_CHAT_ID="chat_id"
+const val KEY_TEXT="text"
+const val SEND_MESSAGE_URL="sendMessage"
+const val KEY_DISABLE_WEB_PAGE_PREVIEW="disable_web_page_preview"
+const val KEY_PARSE_MODE="parse_mode"
+const val MARKDOWN = "markdown"
+const val LINK_MORE_DETAILS ="https://www.oxfordlearnersdictionaries.com/definition/english/"
